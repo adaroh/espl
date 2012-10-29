e63c23ec6071b24df3121e3c8421c747be5b2bdf
Adar Ohana <adaroha@post.bgu.ac.il>
2012-10-29 22:35:47 +0200

    added barcode

--- barcode/encbc.c
+++ barcode/encbc.c
@@ -1,24 +1,24 @@
 #include <assert.h>
 #include <stdio.h>
 #include "bclib.h"
-void printBarcode();
 
 int main(int argc, char **argv)
 {
   int barcode[80][BC_NBITS] = {0};
   int i,j,numOfChar = 0;
+  int currChar;
 
 
   for(i = 1; i < argc; ++i)
   {
 	  for(j = 0;  ; ++j)
 	  {
-		  if(argv[i][j] == '\0')
+		  currChar = argv[i][j];
+		  if(currChar == '\0')
 		  {
 			  break;
 		  }
-		  int bits[BC_NBITS] = {0};
-		  barcode[numOfChar] = bc_char2bits(argv[i][j],bits);
+		  bc_char2bits(currChar,barcode[numOfChar]);
 		  ++numOfChar;
 		
 		  /*printf("<%d , %d > \n",i,j);
