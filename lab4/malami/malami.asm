3c32d168994ca8dd41075117a29357ed51362cc1
Adar Ohana <adaroha@post.bgu.ac.il>
2012-11-27 10:40:52 +0200

    added malami

--- lab4/malami/malami.asm
+++ lab4/malami/malami.asm
@@ -1,3 +1,8 @@
+
+;The function takes a string that represents a number
+; and converts it to integer
+
+
 section .text
 	global malami
 malami:
@@ -7,24 +12,25 @@ malami:
 	mov	DWORD [ebp-4], 0
 	jmp	.L2
 .L3:
-	mov	eax, DWORD [ebp-4]
-	mov ebx, eax
-	sal eax, 3
-	sal ebx, 1
-	add eax, ebx
-	mov	ebx, DWORD [ebp+8]
+	mov	eax, DWORD [ebp-4] ;takes previous sum into eax
+	mov ebx, eax  ; copy to ebx
+	sal eax, 3    ; multiply eax by 8
+	sal ebx, 1 	; multiply ebx by 2	      
+	add eax, ebx	
+	; you have in effect multiplied eax by 10	
+	mov	ebx, DWORD [ebp+8] ;put last char in ebx
 	movzx	ebx, BYTE [ebx]
 	movsx	ebx, bl
-	add	eax, ebx
-	sub	eax, 48
-	mov	DWORD [ebp-4], eax
-	add	DWORD [ebp+8], 1
+	add	eax, ebx  ; add the current char to sum
+	sub	eax, 48   ;48 is 0 in ascii. 
+	mov	DWORD [ebp-4], eax ; move result to sum parameter
+	add	DWORD [ebp+8], 1 ;
 .L2:
 	mov	eax, DWORD [ebp+8]
 	movzx	eax, BYTE [eax]
-	test	al, al
+	test	al, al  ;logic &
 	jne	.L3
-	mov	eax, DWORD [ebp-4]
-	mov esp, ebp
-	pop ebp
-	ret
+	mov	eax, DWORD [ebp-4] ; move resul to eax. eax is always output
+	mov esp, ebp ; go to head of stack
+	pop ebp ; pop beginning 
+	ret ;return eax
