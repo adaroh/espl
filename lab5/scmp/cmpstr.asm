457771f753f399ad03d592c238d56cb66b44c369
Adar Ohana <adaroha@post.bgu.ac.il>
2012-12-04 15:24:17 +0200

    added cmpstr.asm

--- lab5/scmp/cmpstr.asm
+++ lab5/scmp/cmpstr.asm
@@ -10,8 +10,8 @@ cmpstr:
 	mov	ebx,DWORD [ebp+12] ;get the first char of str2
 	movzx	ebx,BYTE [ecx] 
 loop:
-	cmp 	eax, ebx  ;compare char of str1 and str2
-	jne	.NotEqual ;if the chars not same go to .NotEqual
+	cmp 	eax, ebx  ;compare char of str1 with char of str2
+	jne	.NotEqual ;if the chars not the same go to .NotEqual
 	;this block will be done when eax==ebx
 	add	DWORD[ebp+8],1
 	add	DWORD[ebp+12],1  
@@ -22,25 +22,25 @@ loop:
 
 
 .NotEqual:
-	cmp 	eax,ebx   ;compare char of str1 and str2
-	jl	.Less     ;jump to .Less when char in eax<abx
-	jg	.Greater  ;jump to .Less when char in eax>abx
-	je	.Equal 	  ;jump to .Less when char in eax=abx
+	cmp 	eax,ebx   ;compare char of str1 with char of str2
+	jl	.Less     ;jump to .Less when eax<abx
+	jg	.Greater  ;jump to .Less when eax>abx
+	je	.Equal 	  ;jump to .Less when eax=abx
 
 
 .Less:
-	mov	 eax,2 ; update a value to return to 2
+	mov	 eax,2 ; return value = 2
 	jmp	.END
 
 .Greater:
-	mov	eax,1  ; update a value to return to 1
+	mov	eax,1  ; return value = 1
 	jmp	.END
 
 .Equal:
-	mov	eax,0  ; update a value to return to 0
+	mov	eax,0  ; return value = 0
 	jmp	.END
 
 .END:
-	mov	esp,ebp
+	mov	esp,ebp ;ebp pointed to head stack 
 	pop	ebp
 	ret
