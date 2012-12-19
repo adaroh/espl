084f9468a8fe4db91c6e90da2bb901152fd41f4b
Adar Ohana <adaroha@post.bgu.ac.il>
2012-12-19 18:16:23 +0200

    added lab7

---
+++ lab7/bkup/bkup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+fileName=$1
+NCOPIES=$2
+
+if [ ! -e $fileName ] 
+  then
+	echo "file not exists"
+	exit 1
+fi
+
+while [ 0 -ne $NCOPIES ]; do
+    
+    tmpCopys=$(( $NCOPIES - 1 ))
+
+    if [ -r $fileName.$tmpCopys ]
+      then
+	echo "234567"
+	cp $fileName.$tmpCopys $fileName.$NCOPIES
+    fi
+
+    NCOPIES=$(( $NCOPIES - 1 ))
+done
+
+cp $fileName $fileName.1
+
+exit 0
