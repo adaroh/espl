09b5d65573243d37956d20cbb4d7f22242e99990
Adar Ohana <adaroha@post.bgu.ac.il>
2013-01-09 11:57:16 +0200

    added lab 9

--- lab9/hangman/hangman.py
+++ lab9/hangman/hangman.py
@@ -2,6 +2,9 @@
 
 import sys
 import random
+import os
+
+wordlib = "long-words.txt"
 
 def random_line(afile):
     line = next(afile)
@@ -10,9 +13,32 @@ def random_line(afile):
       line = aline
     return line
 
-f = file("long-words.txt")
-if f
+def isFileEmpty(aFile):
+     aFile.seek(0) 
+     first_char = aFile.read(1) #get the first character
+     if not first_char:
+         print "Missing word library" #first character is the empty string..
+         print "(online at http://www.intuitive.com/wicked/examples/long-words.txt save the file as %s and you're ready to play!)\n" %(wordlib)
+         return 1
+     else:
+         aFile.seek(0) #first character wasn't empty, return to start of file.
+         return 0
 
-games = 0
-line = random_line(f)
-print(line)
+
+f = file(wordlib) 
+if isFileEmpty(f) ==0:
+    games = 0
+    word=random_line(f)
+    wrongGuess = 5
+    wordBeforeGuess = ""
+    i=1
+    while i < len(word):
+      wordBeforeGuess += "_ "
+      i +=1
+    
+    print "%s \n %s" %(word,wordBeforeGuess)
+    #while wrongGuess !=0 :
+      
+     # print "Guess a letter: "
+	
+      
